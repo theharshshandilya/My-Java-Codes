9365bd7407b3b54150fd7d0846d9bbc0dd2c8a8c
Harsh <hpc.harsh.22@gmail.com>
2022-09-05 21:13:18 +0530

    abc

---
+++ CodingNinja/Lecture 9/random.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class random {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = s.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
