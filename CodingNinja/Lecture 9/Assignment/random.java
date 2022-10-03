1ad90bc01365805c6676b2e6509b5cd5a81a9caf
Harsh <hpc.harsh.22@gmail.com>
2022-10-03 15:35:34 +0530

    abc

---
+++ CodingNinja/Lecture 9/Assignment/random.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class random {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File();
+        Scanner s = new Scanner(file);
+        int t = s.nextInt();
+        while (t-- > 0) {
+           
+        }
+    }
+}
